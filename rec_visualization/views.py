b7fa3784b6661afea4476389d069f15095a13e98
Andrew <andrew@AndrewdeMacBook-Pro.local>
2016-09-26 19:51:12 +0800

    add .

--- rec_visualization/views.py
+++ rec_visualization/views.py
@@ -29,6 +29,17 @@ def simulate_individual_rec_request(rec_url, key, param):
 
 	return result_list
 
+def query_user_history(history_url, key, param):
+	payload = {key:param}
+	r = requests.get(history_url, params=payload)
+	result_dict = r.json()
+	result_list = []
+	if 'history' in result_dict:
+		for item in result_dict['history']:
+			result_list.append(int(item))
+
+	return result_list
+
 
 def quit_visualize(request):
 	error = False
@@ -73,7 +84,11 @@ def user_visualize(request):
 			video_infos = Sndlvl.objects.filter(id__in=results)
 			video_infos = dict([(video.id, video) for video in video_infos])
 			sorted_video_infos = [video_infos[idx] for idx in results]
-			return render_to_response('quit_visualize.html', {'rec_results':sorted_video_infos, 'lics':vid})
+			history_results = query_user_history('http://10.100.5.104/user_history', 'key', vid)
+			history_videos = Sndlvl.objects.filter(id__in=history_results)
+			history_videos = dict([(video.id, video) for video in history_videos])
+			sorted_history_videos = [history_videos[idx] for idx in history_results]
+			return render_to_response('homePage_visualize.html', {'rec_results':sorted_video_infos, 'lics':vid, 'history':sorted_history_videos})
 	return render_to_response('quit_query.html', {'error': error})
 
 def boot(request):
